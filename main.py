f95c910d8c5981b495deb3121edf042e9e04fc84
ncks03 <nick@schuitenco.nl>
2024-11-07 12:48:36 +0100

    Added users database

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, PositiveInt
+from fastapi.responses import JSONResponse
+
+
+# Classes
+class User(BaseModel):
+    name: str
+    age: PositiveInt
+    gender: str
+    length: PositiveInt
+
+# Databases
+users_db = [{"name": "Pietje", "age": 20, "gender": "M", "length": 182},{"name": "Hendrik", "age": 21, "gender": "M", "length": 190}]
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/users")
+def read_users():
+    return JSONResponse(content=users_db)
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return JSONResponse(content=users_db[user_id])
