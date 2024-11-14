291b74e494f73c5b0c1936fcae0117b4a2980ba5
ncks03 <nick@schuitenco.nl>
2024-11-14 11:30:51 +0100

    Updated entities.py

--- entities/entities.py
+++ entities/entities.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
 ### Database objects ###
