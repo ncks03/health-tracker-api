fef85a54b57fd0fb7ed2f1f093d91969c4ade9d2
ncks03 <nick@schuitenco.nl>
2024-11-14 10:32:27 +0100

    moved functions to seperate script

--- main.py
+++ main.py
@@ -2,19 +2,19 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-from datetime import date
 from fastapi import FastAPI
-from pydantic import BaseModel, PositiveInt, PastDate
 
 ### Imports ###
+from functions import calculate_age
 from entities import *
 import dtos
 
 # Load environment variables
+### DO NOT PUSH .ENV TO GIT ###
 load_dotenv()
 
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_USERNAME = os.getenv("DB_USERNAME") #Insert username variable name here
+DB_PASSWORD = os.getenv("DB_PASSWORD") #Insert password variable name here
 
 # Connection to postgresql Database
 db_connection = psycopg2.connect(
@@ -25,17 +25,10 @@ db_connection = psycopg2.connect(
     port="5432"  # Standard host port
 )
 
-# Functions
-def calculate_age(born):
-    today = date.today()
-    print(today)
-    birth_date = date(year=int(born[0:4]), month=int(born[5:7]), day=int(born[8:10]))
-    return today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
-
 # API Initialisation
 app = FastAPI()
 
-# SQL Query
+# Endpoints definition
 @app.get("/users")
 def get_data_from_db():
     cursor = db_connection.cursor()
