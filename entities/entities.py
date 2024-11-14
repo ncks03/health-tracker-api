ed3b9f76e724df34a186ba093f5a00a8c86b9710
ncks03 <nick@schuitenco.nl>
2024-11-14 11:13:29 +0100

    Integrated alembic migrations

--- entities/entities.py
+++ entities/entities.py
@@ -1,5 +1,9 @@
-import psycopg2
-from sqlalchemy import Base, Column, String, Integer, ForeignKey, Date
+from sqlalchemy import Column, String, Integer, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 ### Database objects ###
 class Customer(Base):
@@ -12,3 +16,26 @@ class Customer(Base):
     gender = Column(String, nullable=False)
     length = Column(Integer, nullable=False)
     activity_level = Column(Integer, nullable=False)
+    goals = relationship("Goal", back_populates="customers")
+
+class Gym(Base):
+    __tablename__ = "gyms"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    customers = relationship("Gym", back_populates="customers")
+    address_place = Column(String, nullable=False)
+
+class Progress(Base):
+    __tablename__ = "progress"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    date = Column(Date, nullable=False)
+    weight = Column(Integer, nullable=False)
+
+class Goal(Base):
+    __tablename__ = "goals"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    weight_goal = Column(Integer, nullable=False)
+    start_date = Column(Date, nullable=False)
+    end_date = Column(Date, nullable=False)
--- main.py
+++ main.py
@@ -2,35 +2,43 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, sessionmaker
 
 ### Imports ###
 from functions import calculate_age
-from entities import *
-import dtos
-
+from entities.entities import Customer as CustomerEntity
+from entities.entities import Gym as GymEntity
+from entities.entities import Progress as ProgressEntity
+from entities.entities import Goal as GoalEntity
+from dtos.dtos import CustomerDTO, GymDTO, ProgressDTO, GoalDTO
 # Load environment variables
 ### DO NOT PUSH .ENV TO GIT ###
 load_dotenv()
 
 DB_USERNAME = os.getenv("DB_USERNAME") #Insert username variable name here
 DB_PASSWORD = os.getenv("DB_PASSWORD") #Insert password variable name here
+DB_URL = os.getenv("DB_URL")
 
 # Connection to postgresql Database
-db_connection = psycopg2.connect(
-    database="health-app-db",
-    user=DB_USERNAME,
-    password=DB_PASSWORD,
-    host="localhost",  # IP Adress of DB host
-    port="5432"  # Standard host port
-)
+# db_connection = psycopg2.connect(
+#     database="health-app-db",
+#     user=DB_USERNAME,
+#     password=DB_PASSWORD,
+#     host="localhost",  # IP Adress of DB host
+#     port="5432"  # Standard host port
+# )
+
+engine = create_engine(DB_URL)
+
+session = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
 
 # API Initialisation
 app = FastAPI()
 
 # Endpoints definition
 @app.get("/users")
-def get_data_from_db():
+def get_user():
     cursor = db_connection.cursor()
     cursor.execute("SELECT * FROM customers")
     data = cursor.fetchall()
@@ -38,6 +46,8 @@ def get_data_from_db():
     return {"data": data}
 
 @app.post("/users")
-def create_user(customer: CustomerDTO):
-    cursor = db_connection.cursor()
-    cursor.execute("INSERT INTO public.users (name, birth_date, gender, length) VALUES (user[name], user[birth_date], user[gender], user[length])")
+def create_user(customer: CustomerDTO, db = Depends(get_db)):
+    customer= CustomerEntity(**customer.dict()) # Create db entity from data
+    db.add(customer) # Add entity to database
+    db.commit() # Commit changes
+    db.refresh(customer) # Refresh database
