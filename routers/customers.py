3697ceace9dd269c12b2fa92d8d9de2eb611db72
ncks03 <nick@schuitenco.nl>
2024-11-19 09:56:34 +0100

    Work on get requests

--- routers/customers.py
+++ routers/customers.py
@@ -6,6 +6,7 @@ from fastapi import Depends, APIRouter, HTTPException
 from dtos.dtos import CustomerDTO
 import entities.entities as entities
 from entities.entities import Customer as CustomerTable
+from entities.entities import Goal as GoalsTable
 
 # Load environment variables
 ### DO NOT PUSH .ENV TO GIT ###
@@ -30,6 +31,7 @@ router = APIRouter(
     tags=["customers"]
 )
 
+### GET REQUESTS ###
 @router.get("/")
 async def read_customers(db = Depends(get_db)):
     result = db.query(CustomerTable).order_by(entities.Customer.id)
@@ -50,12 +52,103 @@ async def read_customer_by_id(customer_id, db = Depends(get_db)):
             "data": list(result)
         }
         return data
+
     except TypeError: #Raise exception for invalid ids
         raise HTTPException(
             status_code=404,
             detail=f"Customer with id {customer_id} not found"
         )
 
+@router.get("/{customer_id}/goals")
+async def read_customer_goals(customer_id, db = Depends(get_db)):
+    try:
+        # Define sqlalchemy statement
+        statement = (
+            select(CustomerTable)
+            .join(GoalsTable)
+            .where(GoalsTable.id==customer_id)
+        )
+        # Execute statement and store result
+        result = db.execute(statement).all()
+        # Store result in data dict
+        data = {
+            "data": list(result)
+        }
+        return data
+
+    except TypeError: #Raise exception for invalid ids
+        raise HTTPException(
+            status_code=404,
+            detail=f"No goals found for customer with id {customer_id}"
+        )
+
+@router.get("/{customer_id}/progress")
+async def read_customer_progress(customer_id, db = Depends(get_db)):
+    try:
+        # Define sqlalchemy statement
+        statement = (
+            select(CustomerTable)
+            .where(CustomerTable.id==customer_id)
+        )
+        # Execute statement and store result
+        result = db.execute(statement).first()
+        # Store result in data dict
+        data = {
+            "data": list(result)
+        }
+        return data
+
+    except TypeError: #Raise exception for invalid ids
+        raise HTTPException(
+            status_code=404,
+            detail=f"Customer with id {customer_id} not found"
+        )
+
+@router.get("/{customer_id}/progress/{progress_id}")
+async def customer_progress_by_id(customer_id, db = Depends(get_db)):
+    try:
+        # Define sqlalchemy statement
+        statement = (
+            select(CustomerTable)
+            .where(CustomerTable.id==customer_id)
+        )
+        # Execute statement and store result
+        result = db.execute(statement).first()
+        # Store result in data dict
+        data = {
+            "data": list(result)
+        }
+        return data
+
+    except TypeError: #Raise exception for invalid ids
+        raise HTTPException(
+            status_code=404,
+            detail=f"Customer with id {customer_id} not found"
+        )
+
+@router.get("/")
+async def read_customer_by_name(customer_name, db = Depends(get_db)):
+    try:
+        # Define sqlalchemy statement
+        statement = (
+            select(CustomerTable)
+            .where(CustomerTable.first_name==customer_name)
+        )
+        # Execute statement and store result
+        result = db.execute(statement).all()
+        # Store result in data dict
+        data = {
+            "data": list(result)
+        }
+        return data
+
+    except TypeError: #Raise exception for invalid ids
+        raise HTTPException(
+            status_code=404,
+            detail=f"Customer with id {customer_id} not found"
+        )
+
+### POST REQUESTS ###
 @router.post("/create_customer")
 async def create_user(customer: CustomerDTO, db = Depends(get_db)):
     customer = CustomerTable(
