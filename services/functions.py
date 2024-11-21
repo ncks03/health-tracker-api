e0159aa296346d6f39349b73cbf507f99ba42eeb
ncks03 <nick@schuitenco.nl>
2024-11-21 13:53:41 +0100

    Moved functions to services directory

--- services/functions.py
+++ services/functions.py
@@ -6,3 +6,6 @@ def calculate_age(born):
     print(today)
     birth_date = date(year=int(born[0:4]), month=int(born[5:7]), day=int(born[8:10]))
     return today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+def calculate_daily_calorie_intake():
+    pass
