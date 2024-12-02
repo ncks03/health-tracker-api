0bfc00a52e412f46406fe0c82f0940066dd40afc
ncks03 <nick@schuitenco.nl>
2024-12-02 13:03:02 +0100

    Changed test file names for pytest functionality

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
-from routers.customers import read_customer_by_name, CustomerResponse
+from routers.customers import read_customer_by_name, read_customer_by_id, get_daily_calorie_intake
+from schemas.responses import CustomerResponse, SingleCustomerResponse
 from datetime import datetime
 
 # Create mock data directly as CustomerResponse objects
@@ -32,6 +33,13 @@ mock_customers = [
     )
 ]
 
+mock_customer = SingleCustomerResponse(
+id=1, first_name='John', last_name='Doe', gender='male',
+        birth_date=datetime(1990, 1, 1).date(), length=180, weight=80,
+        gym_id=1, activity_level=10.0
+)
+
+# Test get requests
 @pytest.mark.asyncio
 async def test_get_customers():
     # Arrange
@@ -47,3 +55,44 @@ async def test_get_customers():
     # Directly comparing the result to the mock data
     assert result == {'customers': mock_customers}
     #nu wel goeie push?....
+
+@pytest.mark.asyncio
+async def test_customer_by_id():
+    # Arrange
+    mock_db = MagicMock()
+
+    # Return the mock customers directly when the query is executed
+    mock_db.execute.return_value.scalars.return_value.first.return_value = mock_customers[0]
+    (mock_db.query.return_value
+     .filter.return_value
+     .order_by.return_value
+     .limit.return_value.scalar).return_value = 80
+
+    # Act
+    result = await read_customer_by_id(1, db=mock_db)
+
+    # Assert
+    # Directly comparing the result to the mock data
+    assert result == {'data': mock_customer}
+
+@pytest.mark.asyncio
+async def test_daily_calorie_intake():
+    # Arrange
+    mock_db = MagicMock()
+
+    # Act
+    result = await get_daily_calorie_intake(1, db=mock_db)
+
+    # Assert
+    # Directly comparing the result to the mock data
+    assert result == "This function does not work yet"
+
+# Test post requests
+@pytest.mark.asyncio
+async def test_create_user():
+    #Arrange
+    mock_db = MagicMock()
+
+    #Act
+
+    #Assert
