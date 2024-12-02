790f8e50541b466c2dd8d42934b35e37335beb49
ncks03 <nick@schuitenco.nl>
2024-12-02 08:52:16 +0100

    Fixed error in pytest: no module found

--- main.py
+++ main.py
@@ -27,6 +27,10 @@ def get_db():
 
 # API Initialisation
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return "the server is up and running"
 app.include_router(customers.router)
 app.include_router(gyms.router)
 app.include_router(goals.router)
