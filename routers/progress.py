fa57a22f0f9f77c0f42c161eed267b8d53dbb1c7
ncks03 <nick@schuitenco.nl>
2024-12-05 12:49:04 +0100

    updated get_db function in progress to use global function

--- routers/progress.py
+++ routers/progress.py
@@ -8,26 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from fastapi import Depends, APIRouter, HTTPException
 from schemas.dtos import ProgressDTO
 from models.entities import Gym, Progress
-
-# Load environment variables
-### DO NOT PUSH .ENV TO GIT ###
-load_dotenv()
-
-DB_USERNAME = os.getenv("DB_USERNAME") #Insert username variable name here
-DB_PASSWORD = os.getenv("DB_PASSWORD") #Insert password variable name here
-DB_URL = os.getenv("DB_URL")
-
-# Connection to postgresql Database
-engine = create_engine(DB_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from services.functions import get_db
 
 router = APIRouter(
     prefix="/progress",
